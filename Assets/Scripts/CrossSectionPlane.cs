ef6726abf00ed3fc1856af321dd4a3555eb20c6a
Henri J. Norden <55378880+Henri-J-Norden@users.noreply.github.com>
2022-03-28 21:51:59 +0200

    Add 3-plane versions; Update to 2019.4

--- Assets/Scripts/CrossSectionPlane.cs
+++ Assets/Scripts/CrossSectionPlane.cs
@@ -3,24 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //[ExecuteInEditMode]
-public class CrossSectionPlane : MonoBehaviour
-{
-    public Material mat1, mat2;
-    public float KeyMoveSpeed = 1;
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.position += transform.forward * KeyMoveSpeed * Time.deltaTime;
-        }
-        else if(Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.position -= transform.forward * KeyMoveSpeed * Time.deltaTime;
-        }
-        mat1.SetVector("_PlanePosition", transform.position);
-        mat1.SetVector("_PlaneNormal", transform.forward);
-        mat2.SetVector("_PlanePosition", transform.position);
-        mat2.SetVector("_PlaneNormal", transform.forward);
-    }
+public class CrossSectionPlane : MonoBehaviour {
+
+	public string plane = "1";
+	public List<Material> materials = new List<Material>();
+	public float KeyMoveSpeed = 1;
+
+	void Update() {
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			transform.position += transform.forward * KeyMoveSpeed * Time.deltaTime;
+		} else if (Input.GetKey(KeyCode.DownArrow)) {
+			transform.position -= transform.forward * KeyMoveSpeed * Time.deltaTime;
+		}
+
+		foreach (var material in materials) {
+			material.SetVector($"_Plane{plane}Position", transform.position);
+			material.SetVector($"_Plane{plane}Normal", transform.forward);
+		}
+	}
 }
